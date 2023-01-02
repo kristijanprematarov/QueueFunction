f5fb2bee514d459abdfdaf9ca1303959ae857527
Kristijan Prematarov <Kristijan.Prematarov@opengi.mk>
2023-01-02 21:09:13 +0100

    Object input type, ex: Order, Azure function to Azure Table

--- QueueFunction/Queue.cs
+++ QueueFunction/Queue.cs
@@ -5,13 +5,42 @@ using Microsoft.Extensions.Logging;
 
 namespace QueueFunction
 {
+    public class Order
+    {
+        public string OrderID { get; set; }
+        public int Quantity { get; set; }
+    }
+
+    public class TableOrder
+    {
+        #region NEEDED FOR AZURE TABLE STORAGE
+        public string PartitionKey { get; set; }
+        public string RowKey { get; set; }
+        #endregion
+
+    }
+
     public class Queue
     {
+        //return => WE USE THIS TO REFER THAT THE TRIGGER WILL WRITE TO TABLE STORAGE
+        //CONNECTION IS THE SAME BECAUSE OF THE SAME STORAGE ACCOUNT
         [FunctionName("GetMessages")]
-        public void Run([QueueTrigger("appqueue", Connection = "connectionString")]string myQueueItem, ILogger log)
+        [return: Table("Orders", Connection = "connectionString")]
+        public TableOrder Run([QueueTrigger("appqueue", Connection = "connectionString")] Order order, ILogger log)
         {
+            TableOrder tableOrder = new TableOrder()
+            {
+                PartitionKey = order.OrderID,
+                RowKey = Convert.ToString(order.Quantity)
+            };
+
             //EXPECTS THAT MESSAGES WERE SENT AS BASE64ENCODED
-            log.LogInformation($"C# Queue trigger function processed: {myQueueItem}");
+            //log.LogInformation($"ORDER ID: {order.OrderID}");
+            //log.LogInformation($"QUANTITY: {order.Quantity}");
+
+            log.LogInformation($"Order information has been written to AzureTableStorage");
+
+            return tableOrder;
         }
     }
 }
